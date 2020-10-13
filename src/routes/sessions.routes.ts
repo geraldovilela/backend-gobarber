fcf2a68d14f7d932490cdb70daf30e4a5343e908
geraldo vilela <geraldovilela19@gmail.com>
2020-10-13 18:24:17 -0400

    dev: session route/service setting

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionRouter = Router();
+
+sessionRouter.post('/', async (request, response) => {
+  try {
+    const { login, email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await authenticateUser.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user, token });
+  } catch (err) {
+    return response
+      .status(400)
+      .json({ error: err.message, custom: 'erro at session Post' });
+  }
+});
+
+export default sessionRouter;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign(
+      { user: user.name },
+      '05a83567cc00f0d997ab98412ca80536',
+      { subject: user.id, expiresIn: '8h' },
+    );
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
