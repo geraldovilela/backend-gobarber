59e99a78ed84b30ef97c17b5a2b0f90ee3a6e210
geraldo vilela <geraldovilela19@gmail.com>
2020-10-13 18:10:44 -0400

    dev: migration adjust

--- src/database/migrations/1602437549356-CreateAppointments.ts
+++ src/database/migrations/1602437549356-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1602437549356
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
